dba1a10a8346edc1f43171d22204180182f2ef05
Leonard Truong <lennytruong@berkeley.edu>
2015-02-08 16:54:39 -0800

    Update server/client code

--- app/led_server.lua
+++ app/led_server.lua
@@ -7,9 +7,8 @@ brd = LED:new("GP0")
 server = function()
    ssock = storm.net.udpsocket(7, 
 			       function(payload, from, port)
-				  print(string.format("from %s port %d: %s",from,port,payload))
                                   local message = storm.mp.unpack(paylod)
-				  brd:flash(1)
+				  print(string.format("temperature is: ", message.temp))
 			       end)
 end
 
--- app/temp-sensor-client.lua
+++ app/temp-sensor-client.lua
@@ -1,4 +1,5 @@
 require("cord")
+temp_sensor = require("temp_sensor")
 
 cport = 49152
 csock = storm.net.udpsocket(cport, 
@@ -6,10 +7,16 @@ csock = storm.net.udpsocket(cport,
 			       print(string.format("echo from %s port %d: %s",from,port,payload))
 			    end)
 
-local temperature_monitor = storm.os.invokePeriodically(storm.os.MILLISECOND * 10, function ()
-  local temp = temp_sensor.get_temp()
-  local message = storm.mp.pack(temp)
-  storm.net.sendto(csock, message, "ff02::1", 7)
+local sensor = temp_sensor:new()
+
+local temperature_monitor = storm.os.invokePeriodically(storm.os.MILLISECOND * 100, function ()
+  cord.new(function() 
+    local temp = sensor:get()
+    local msg = {}
+    msg.temp = temp
+    local message = storm.mp.pack(msg)
+    storm.net.sendto(csock, message, "ff02::1", 7)
+  end)
 end)
 
 cord.enter_loop()
