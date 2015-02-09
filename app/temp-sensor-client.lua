caed63826d2f7d48c081b265bbbf74be93a54348
Leonard Truong <lennytruong@berkeley.edu>
2015-02-08 17:47:17 -0800

    Update temp sensor client.

--- app/temp-sensor-client.lua
+++ app/temp-sensor-client.lua
@@ -8,14 +8,27 @@ csock = storm.net.udpsocket(cport,
 			    end)
 
 local sensor = temp_sensor:new()
-
-local temperature_monitor = storm.os.invokePeriodically(storm.os.MILLISECOND * 100, function ()
+local last_temp = nil
+local temperature_monitor = storm.os.invokePeriodically(storm.os.MILLISECOND * 500, function ()
   cord.new(function() 
-    local temp = sensor:get()
-    local msg = {}
-    msg.temp = temp
-    local message = storm.mp.pack(msg)
-    storm.net.sendto(csock, message, "ff02::1", 7)
+    local curr_temp = sensor:get()
+    if not last_temp then
+       last_temp = curr_temp
+    end
+    print("last_temp: ", last_temp)
+    print("curr_temp: ", curr_temp)
+    local difference = last_temp - curr_temp
+    if difference < 0 then
+       difference = -1 * difference
+    end
+    print("difference is: ", difference)
+    if difference > 4 then
+      local msg = {}
+      msg.temp = curr_temp
+      local message = storm.mp.pack(msg)
+      storm.net.sendto(csock, message, "ff02::1", 7)
+      last_temp = curr_temp
+    end
   end)
 end)
 
