4c26e46557f5a9516cc1970684361e5b44d3a83f
Cesar Torres <cearto@berkeley.edu>
2015-02-08 16:05:38 -0800

    correct temp calc

--- lib/tempcalc.lua
+++ lib/tempcalc.lua
@@ -8,6 +8,8 @@ function calc_temp(temp, volt)
 	local b1 = -5.7e-7
 	local b2 = 4.63e-9
 	local c2 = 13.4
-	S = 1 + (a1 * (temp - tref)  + a2 * ((temp - tref) * (temp - tref)))
-	return math.pow(math.pow(temp, 4) + volt/S), 1/4)
+	S = 1 + (a1 * (temp - tref)  + a2 * math.pow(temp-tref, 2)
+	Vos = b0 + b1 * (temp - tref) + b2 * math.pow(temp-tref, 2)
+	f = (volt - Vos) + c2 * math.pow(volt- Vos, 2)
+	return math.pow(math.pow(temp, 4) + f/S, 1/4)
 end
